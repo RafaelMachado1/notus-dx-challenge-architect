3cd8dfee7cbc1dfce4d4f7935ea8e513f3841508
Rafael Machado <rafaelmartinsmachado25@gmail.com>
2025-10-02 19:07:09 -0300

    feat(sdk): adiciona assinaturas dos métodos do SDK para Trilha A conforme testes realizados

---
+++ src/sdk/client.ts
@@ -0,0 +1,12 @@
+export async function getChains(payload: any) {}
+export async function getTokens(payload: any) {}
+
+export async function registerSmartWallet(payload: any) {}
+export async function getSmartWallet(payload: any) {}
+export async function getSmartWalletHistory(payload: any) {}
+export async function getSmartWalletPortfolio(payload: any) {}
+export async function createDepositTransaction(payload: any) {}
+// Não implementar updateTransactionMetadata, pois não foi testado
+
+export async function createKYCSession(payload: any) {}
+export async function getKYCResult(payload: any) {}
